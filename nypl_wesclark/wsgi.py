1d45a5755d9c7fcac6d0c4595772be986dc0a510
mostlywes <98903226+mostlywes@users.noreply.github.com>
2022-02-02 13:56:17 -0500

    Update wsgi.py

--- nypl_wesclark/wsgi.py
+++ nypl_wesclark/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nypl_wesclark.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nypl_wesclark.nypl_wesclark.settings')
 
 application = get_wsgi_application()
